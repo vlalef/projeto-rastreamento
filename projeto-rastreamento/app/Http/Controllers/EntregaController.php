465837706443eb2872e886d046a20cb174999fa7
vlalef <vaz.alef@proton.me>
2025-01-26 20:37:28 -0300

    feat: adding a status from http protocol

--- projeto-rastreamento/app/Http/Controllers/EntregaController.php
+++ projeto-rastreamento/app/Http/Controllers/EntregaController.php
@@ -9,10 +9,12 @@ class EntregaController extends Controller
 {
     public function getEntregasByCpf($cpf): \Illuminate\Http\JsonResponse
     {
-        $entregas = Entrega::where('destinatario->cpf', $cpf)
-            ->with('transportadora')
-            ->get();
+        $entregas = Entrega::where('cpf', $cpf)->with('transportadora')->get();
 
-        return response()->json($entregas);
+        if ($entregas->isEmpty()) {
+            return response()->json(['message' => 'No deliveries found for the provided CPF'], 404);
+        }
+
+        return response()->json($entregas, 200);
     }
 }
